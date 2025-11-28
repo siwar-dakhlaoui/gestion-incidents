24d5c5c52769423e881c3e6e5c5052d73ffd28d9
siwar-dakhlaoui <siwardkhlaoui12@gmail.com>
2025-11-28 13:15:05 +0100

    Initial commit: Spring Boot project for smart city incident management

---
+++ src/main/java/com/isamm/gestion_incidents/GestionIncidentsApplication.java
@@ -0,0 +1,13 @@
+package com.isamm.gestion_incidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionIncidentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionIncidentsApplication.class, args);
+	}
+
+}
